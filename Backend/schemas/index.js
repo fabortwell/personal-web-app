6b1fbc27dc25c58774796a18c30ef883be4e7365
user <tre@Brian.local>
2023-01-13 16:07:55 +0300

    initialized sanity schemas with relavant information

--- Backend/schemas/index.js
+++ Backend/schemas/index.js
@@ -1,2 +1,14 @@
-export const schemaTypes = [testimonials]
+export const schemaTypes = [testimonials, abouts, contact, brands, experiences, workExperiences, skills, works]
 import testimonials from "./testimonials"
+import abouts from "./abouts"
+import contact from "./contact"
+import brands from "./brands"
+import experiences from "./experiences"
+import workExperiences from "./workExperiences"
+import skills from "./skills"
+import works from "./works"
+
+
+
+
+
