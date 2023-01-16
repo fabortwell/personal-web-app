cfd436309a2f49b0da9840ab9600a5c1e6eb5089
user <tre@Brian.local>
2023-01-16 17:09:43 +0300

    added media query to hero section

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -18,7 +18,7 @@ const scaleVariants = {
 
 const Header = () => {
   return (
-    <div className="app__header app__flex">
+    <div id="home" className="app__header app__flex">
       <motion.div
          whileInView={{ x: [-100, 0], opacity: [0, 1]}}
          transition={{ duration: 0.5 }}
@@ -64,7 +64,7 @@ const Header = () => {
 
 {[images.javascript, images.redux, images.sass].map((circle, index) => (
         <div className="circle-cmp app__flex" key={`circle-${index}`}>
-          <img src={circle} alt="profile_bg" />
+          <img src={circle} alt="circle" />
         </div>
 ))}
 </motion.div>
